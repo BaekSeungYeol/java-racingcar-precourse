6bbda8af9683ac1c1dfcbf77f903f9ee2b9c42d5
백승열 <beck33333@naver.com>
2021-05-01 22:56:12 +0900

    feat : 각 자동차의 이름을 부여하는 클래스

---
+++ src/main/java/racingcar/CarName.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+public class CarName {
+
+    private final String name;
+    public CarName(String name) {
+        checkEnglish(name, "이름은 영어여야 합니다.");
+
+        this.name = name;
+    }
+
+    private void checkEnglish(String name, String message) {
+        for(int i=0; i< name.length(); ++i) {
+            validAlp(name.charAt(i), message);
+        }
+    }
+
+    public static void validAlp(char alp, String message) {
+        if(alp < 'a' || alp > 'z') {
+            throw new IllegalArgumentException(message);
+        }
+
+    }
+
+}
