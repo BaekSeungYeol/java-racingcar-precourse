af2be867b3edd2cf771bf8f03c4ea4067c8c75d7
백승열 <beck33333@naver.com>
2021-05-01 22:55:47 +0900

    test : 유저가 적은 이름이 영어 인지 테스트

---
+++ src/test/java/CarNameTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.BoostNumber;
+import racingcar.CarName;
+
+public class CarNameTest {
+
+    @Test
+    void validAlp() {
+        char alp = 'a';
+        CarName.validAlp(alp,"이름은 영어여야 합니다.");
+    }
+
+    @Test
+    void validNameSuccess() {
+        String name = "seungyeol";
+        CarName carName = new CarName(name);
+    }
+    @Test
+    void validNameFailed() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new CarName("1234"),
+                () -> "이름은 영어여야 합니다.");
+    }
+
+
+}
