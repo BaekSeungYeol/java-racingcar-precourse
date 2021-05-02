78a8dc2039214675b51eccf75be6961c650c7971
백승열 <beck33333@naver.com>
2021-05-02 22:46:13 +0900

    feat: ui 기능 추가

---
+++ src/main/java/racingcar/ui/PrintUtils.java
@@ -0,0 +1,23 @@
+package racingcar.ui;
+
+import racingcar.model.Cars;
+
+public class PrintUtils {
+
+    public static void start() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+    }
+
+    public static void tryTime() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+
+    public static void printWinners(String... winners) {
+        for(int i=0; i< winners.length; ++i) {
+            System.out.print(winners[i] + " ");
+        }
+        System.out.println("가 최종 우승 했습니다.");
+    }
+}
