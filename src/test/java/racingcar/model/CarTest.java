0cb0856ee50b62be7bc173a1bcc8bb91721a5d53
백승열 <beck33333@naver.com>
2021-05-02 22:45:17 +0900

    test: 전진하는 조건의 숫자가 4이상과 미만일때

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,27 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+    @Test
+    @DisplayName("전진_하는_조건의_숫자가_4이상")
+    void GoBoostNumber() {
+        int randomNumber = 4;
+        Car car = new Car("seung",0);
+        car.updateBoostNumber(randomNumber);
+        assertThat(car.getNumber()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("전진_하는_조건의_숫자가_4미만")
+    void StopBoostNumber() {
+        int randomNumber = 3;
+        Car car = new Car("seung",0);
+        car.updateBoostNumber(randomNumber);
+        assertThat(car.getNumber()).isEqualTo(0);
+    }
+
+}
