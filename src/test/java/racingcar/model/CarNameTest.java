7206157ae5c4eb1473eba40b269dbc5ab8c0f04e
백승열 <beck33333@naver.com>
2021-05-02 22:53:40 +0900

    test: 가독성을 위한 DisplayName 추가

--- src/test/java/racingcar/model/CarNameTest.java
+++ src/test/java/racingcar/model/CarNameTest.java
@@ -1,22 +1,26 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarNameTest {
 
     @Test
+    @DisplayName("알파벳_영어_체크")
     void validAlp() {
         char alp = 'a';
         CarName.validAlp(alp,"이름은 영어여야 합니다.");
     }
 
     @Test
+    @DisplayName("영어_이름_체크")
     void validNameSuccess() {
         String name = "seungyeol";
         CarName carName = new CarName(name);
     }
     @Test
+    @DisplayName("영어_이름_체크_실패")
     void validNameFailed() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new CarName("1234"),
--- src/test/java/racingcar/model/TryTimeTest.java
+++ src/test/java/racingcar/model/TryTimeTest.java
@@ -1,17 +1,20 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TryTimeTest {
 
 
     @Test
+    @DisplayName("올바른_횟수_입력")
     void validTrySuccess() {
         TryTime time = new TryTime(5);
     }
 
     @Test
+    @DisplayName("올바르지않는_횟수_입력")
     void validTryFailed() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new TryTime(-1),
